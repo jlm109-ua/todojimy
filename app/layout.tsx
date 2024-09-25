82ccd4026f99b82d4e68480979e5fc2d18345aab
jimy <jimy@dev.dev.org>
2024-09-25 11:44:13 +0200

    Icon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import Head from 'next/head';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -17,7 +18,9 @@ export const metadata: Metadata = {
   title: 'ToDoJimy',
   description: "Jimy's personal ToDo App",
   icons: {
-    icon: '/todojimy.png',
+    icon: [
+      { url: '/todojimy.png', sizes: 'any' }
+    ],
   },
 };
 
@@ -28,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/todojimy.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
